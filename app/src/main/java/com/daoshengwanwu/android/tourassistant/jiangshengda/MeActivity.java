e1d35d43b9c318b2efdcf18d71c51cf1f5edd072
Santa <lilin.santa@gmail.com>
2016-11-24 10:31:59 +0800

    实现“我”页面、“搜索页面” 圆形头像效果实现

--- app/src/main/java/com/daoshengwanwu/android/tourassistant/jiangshengda/MeActivity.java
+++ app/src/main/java/com/daoshengwanwu/android/tourassistant/jiangshengda/MeActivity.java
@@ -1,11 +1,12 @@
 package com.daoshengwanwu.android.tourassistant.jiangshengda;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.daoshengwanwu.android.tourassistant.R;
 
-public class MeActivity extends AppCompatActivity {
+public class MeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

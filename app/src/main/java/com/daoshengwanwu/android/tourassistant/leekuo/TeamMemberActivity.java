059029e53d4e473cb341c6c89ccbb88655fd4522
leekuo3 <leekuo3@126.com>
2016-11-24 18:05:45 +0800

    “我的队伍-队长”、“转让队伍”、“我的队伍-组员”、“小组成员信息”  页面的编写已完成

---
+++ app/src/main/java/com/daoshengwanwu/android/tourassistant/leekuo/TeamMemberActivity.java
@@ -0,0 +1,16 @@
+package com.daoshengwanwu.android.tourassistant.leekuo;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.daoshengwanwu.android.tourassistant.R;
+
+public class TeamMemberActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lk_activity_team_member);
+    }
+}

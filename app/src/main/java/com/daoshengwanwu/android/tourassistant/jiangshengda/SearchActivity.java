4abba0222858cd773b6ac1e20e9d6dc6ba4b9197
Santa <lilin.santa@gmail.com>
2016-12-12 15:50:11 +0800

    蒋胜达负责页面添加跳转代码

--- app/src/main/java/com/daoshengwanwu/android/tourassistant/jiangshengda/SearchActivity.java
+++ app/src/main/java/com/daoshengwanwu/android/tourassistant/jiangshengda/SearchActivity.java
@@ -1,16 +1,21 @@
 package com.daoshengwanwu.android.tourassistant.jiangshengda;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.daoshengwanwu.android.tourassistant.R;
+import com.daoshengwanwu.android.tourassistant.shenyue.ScenicspotActivity;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class SearchActivity extends Activity {
     private TextView log;
@@ -35,6 +40,16 @@ public class SearchActivity extends Activity {
         lv = (ListView) findViewById(R.id.Lv);
         log = (TextView) findViewById(R.id.His_log);
         lv.setAdapter(mAdapter);
+        //每个Item的点击事件
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (ls.get(position).equals(1)){
+                    ScenicspotActivity.actionStartActivity(this, UUID.randomUUID().toString());
+                }
+
+            }
+        });
         btn = (RelativeLayout) findViewById(R.id.Clean_history);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +59,11 @@ public class SearchActivity extends Activity {
                 log.setText("暂无历史记录");
             }
         });
+    }
+
+    public static  void actionStartActivity(Context packageContext) {
+        Intent intent = new Intent(packageContext, SearchActivity.class);
+        packageContext.startActivity(intent);
     }
 
 }

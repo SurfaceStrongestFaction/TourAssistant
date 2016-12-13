3648c6c6d0899c71a69fef2ff2feff26dc4ccdb9
leekuo3 <leekuo3@126.com>
2016-12-13 09:36:24 +0800

    我的队伍、转让队伍、我的信息页面之间的跳转和我的信息——》发消息——》蔡子欢的ChatActivity 的页面跳转

--- app/src/main/java/com/daoshengwanwu/android/tourassistant/leekuo/TeamMemberActivity.java
+++ app/src/main/java/com/daoshengwanwu/android/tourassistant/leekuo/TeamMemberActivity.java
@@ -6,13 +6,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.daoshengwanwu.android.tourassistant.R;
+import com.daoshengwanwu.android.tourassistant.caizihuan.ChatActivity;
 
 public class TeamMemberActivity extends BaseActivity {
     private static final String EXTRA_USER_NAME = "SecondActivity.EXTRA_USER_NAME";
     private ImageView back;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +28,17 @@ public class TeamMemberActivity extends BaseActivity {
                 TeamMemberActivity.this.finish();
             }
         });
+        button1=(Button)findViewById(R.id.myTeam_button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChatActivity.startChatActivity(TeamMemberActivity.this);
+            }
+        });
     }
 
-    public static Intent newIntent(Context packageContext, String userName) {
-        Intent i = new Intent(packageContext, TeamMemberActivity.class);
-        i.putExtra(EXTRA_USER_NAME, userName);
-        return i;
+    public static  void actionStartActivity(Context packageContext) {
+        Intent intent = new Intent(packageContext, TeamMemberActivity.class);
+        packageContext.startActivity(intent);
     }
 }
--- app/src/main/java/com/daoshengwanwu/android/tourassistant/shenyue/PictureActivity.java
+++ app/src/main/java/com/daoshengwanwu/android/tourassistant/shenyue/PictureActivity.java
@@ -4,10 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.GridView;
-import android.widget.ImageView;
-
 import com.daoshengwanwu.android.tourassistant.R;
 import com.daoshengwanwu.android.tourassistant.leekuo.BaseActivity;
 
@@ -15,11 +12,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PictureActivity extends BaseActivity {
-    private static final String PICTUREEXTRA_ID = "PictureActivity.EXTRA_ID";
+
     List<PicgvItem> ls = new ArrayList<>();
     GridView picgv;
     PicturegvAdapter picturegvAdapter;
-    ImageView backimg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,12 +25,6 @@ public class PictureActivity extends BaseActivity {
         getData();
         picturegvAdapter = new PicturegvAdapter(this, ls);
         picgv.setAdapter(picturegvAdapter);
-        getIntent().getIntExtra(PICTUREEXTRA_ID, 0);
-    }
-    public static void actionStartActivity(Context packageContext, String id) {
-        Intent i = new Intent(packageContext, PictureActivity.class);
-        i.putExtra(PICTUREEXTRA_ID, id);
-        packageContext.startActivity(i);
     }
     /**
      * 添加数据
@@ -50,22 +40,8 @@ public class PictureActivity extends BaseActivity {
         ls.add(new PicgvItem(1, R.drawable.picture8));
         ls.add(new PicgvItem(1, R.drawable.picture9));
         ls.add(new PicgvItem(1, R.drawable.picture10));
+
     }
-    /**
-     * 获取界面控件
-     */
-    private void getViews(){
-        backimg = (ImageView) findViewById(R.id.picture_back_img);
-    }
-    /**
-     * 注册事件监听器
-     */
-    private void setListener(){
-        backimg.setOnClickListener(new MyListener());
-    }
-    class MyListener implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
 
     public static Intent newIntent(Context packageContext) {
         Intent i = new Intent(packageContext, PictureActivity.class);
